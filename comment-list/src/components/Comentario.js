42ad038e1ba3ebecb216524b3340c265920d14e7
Nathan <nathansaraujo191@gmail.com>
2022-01-07 15:27:56 -0300

    commit inicial

---
+++ comment-list/src/components/Comentario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comentario.css'
+
+const Comentario = () => (
+    <div className='comentario'>
+        <div>João</div>
+        <div>Olá, eu sou um componente</div>
+    </div>
+);
+
+export default Comentario;
