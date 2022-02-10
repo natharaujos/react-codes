126c20163c096997e3025bb58aa4748c4b66f9f5
Nathan <nathansaraujo191@gmail.com>
2022-02-10 10:28:05 -0300

    adicionando código da barra de progresso

---
+++ progressbar/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Progress from "./Progress";
+
+const App = () => {
+	
+	return (
+		<div>
+			<h1>React Progress Bar</h1>
+			<Progress done="100"/>
+		</div>
+	)
+}
+
+export default App;
