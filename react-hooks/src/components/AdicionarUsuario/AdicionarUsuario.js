a60034206c0e2a9e270ed0e21bd560dcb564e91e
Nathan <nathansaraujo191@gmail.com>
2022-03-07 15:18:05 -0300

    adicionando algumas modificações no react=hooks (transformando os componentes de classe do react-http usando o useSate e useEffect)

--- react-hooks/src/components/AdicionarUsuario/AdicionarUsuario.js
+++ react-hooks/src/components/AdicionarUsuario/AdicionarUsuario.js
@@ -1,86 +1,76 @@
-import React from 'react'
-
-import './AdicionarUsuario.css'
-
-const INITIAL_STATE = { 
-  usuario: { nome: '', sobrenome: '', email: '' } 
-}
+import React, { useState } from "react";
+import "./AdicionarUsuario.css";
 
 function AdicionarUsuario(props) {
-    this.state = INITIAL_STATE
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [email, setEmail] = useState("");
 
-    this.onChangeHandler = this.onChangeHandler.bind(this)
-    this.onSubmitHandler = this.onSubmitHandler.bind(this)
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    this.setState({ usuario: { ...this.state.usuario, [name]: value } });
+  };
 
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
 
-  onChangeHandler(event) {
-    const { name, value } = event.target
-    this.setState({ usuario: { ...this.state.usuario, [name]: value } })
-  }
+    const usuario = { nome, sobrenome, email };
 
-  onSubmitHandler(event) {
-    event.preventDefault()
-   
-    const usuario = this.state.usuario
-
-    fetch('https://reqres.in/api/users', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(usuario)
+    fetch("https://reqres.in/api/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(usuario),
     })
-      .then(resposta => resposta.json())
-      .then(dados => {
-        this.setState(INITIAL_STATE)
-        this.props.adicionarUsuario(dados)
-      })
-  }
-
-  render() {
-    return (
-      <div className="AdicionarUsuario">
-        <h2>Adicionar Usuário</h2>
-        <form onSubmit={this.onSubmitHandler}>
-          <div className="Linha">
-            <div className="Coluna">
-              <label>Nome</label>
-              <input
-                type="text"
-                name="nome"
-                value={this.state.usuario.nome}
-                onChange={this.onChangeHandler}
-                required>
-              </input>
-            </div>
-            <div className="Coluna">
-              <label>Sobrenome</label>
-              <input
-                type="text"
-                name="sobrenome"
-                value={this.state.usuario.sobrenome}
-                onChange={this.onChangeHandler}
-                required>
-              </input>
-            </div>
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        setNome("");
+        setSobrenome("");
+        setEmail("");
+        props.adicionarUsuario(dados);
+      });
+  };
+  return (
+    <div className="AdicionarUsuario">
+      <h2>Adicionar Usuário</h2>
+      <form onSubmit={onSubmitHandler}>
+        <div className="Linha">
+          <div className="Coluna">
+            <label>Nome</label>
+            <input
+              type="text"
+              name="nome"
+              value={nome}
+              onChange={(event) => setNome(event.target.value)}
+              required
+            ></input>
           </div>
-          <div className="Linha">
-            <div className="Coluna">
-              <label>Email</label>
-              <input
-                type="email"
-                name="email"
-                value={this.state.usuario.email}
-                onChange={this.onChangeHandler}
-                required>
-              </input>
-            </div>
+          <div className="Coluna">
+            <label>Sobrenome</label>
+            <input
+              type="text"
+              name="sobrenome"
+              value={sobrenome}
+              onChange={(event) => setSobrenome(event.target.value)}
+              required
+            ></input>
           </div>
-          <button type="submit">
-            Adicionar
-        </button>
-        </form>
-      </div>
-    )
-  }
+        </div>
+        <div className="Linha">
+          <div className="Coluna">
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            ></input>
+          </div>
+        </div>
+        <button type="submit">Adicionar</button>
+      </form>
+    </div>
+  );
 }
 
-export default AdicionarUsuario
+export default AdicionarUsuario;
--- react-hooks/src/components/Usuarios/Usuarios.js
+++ react-hooks/src/components/Usuarios/Usuarios.js
@@ -1,66 +1,59 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from "react";
 
-import AdicionarUsuario from '../AdicionarUsuario/AdicionarUsuario'
-import Usuario from '../Usuario/Usuario'
+import AdicionarUsuario from "../AdicionarUsuario/AdicionarUsuario";
+import Usuario from "../Usuario/Usuario";
 
-class Usuarios extends Component {
+function Usuarios(props) {
+  const [usuarios, setUsuarios] = useState([]);
 
-  constructor(props) {
-    super(props)
-    this.state = { usuarios: [] }
-
-    this.adicionarUsuario = this.adicionarUsuario.bind(this)
-  }
-
-  adicionarUsuario(usuario) {
-    const usuarios = [...this.state.usuarios, usuario]
-    this.setState({ usuarios: usuarios })
-  }
-
-  removerUsuario(usuario) {
-    if (window.confirm(`Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`)) {
-      fetch(`https://reqres.in/api/users/${usuario.id}`, {
-        method: 'DELETE'
-      })
-        .then(resposta => {
-          if (resposta.ok) {
-            let usuarios = this.state.usuarios
-            usuarios = usuarios.filter(x => x.id !== usuario.id)
-            this.setState({ usuarios: usuarios })
-          }
-        })
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://reqres.in/api/users')
-      .then(resposta => resposta.json())
-      .then(dados => {
-        const usuarios = dados.data.map(usuario => ({
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((resposta) => resposta.json())
+      .then((dados) => {
+        const usuarios = dados.data.map((usuario) => ({
           id: usuario.id,
           nome: usuario.first_name,
           sobrenome: usuario.last_name,
-          email: usuario.email
-        }))
+          email: usuario.email,
+        }));
 
-        this.setState({ usuarios })
-      })
-  }
+        setUsuarios(usuarios);
+      });
+  }, []);
 
-  render() {
-    return (
-      <>
-        <AdicionarUsuario adicionarUsuario={this.adicionarUsuario} />
+  const adicionarUsuario = (usuario) => {
+    setUsuarios((usuariosAtuais) => [...usuariosAtuais, usuario]);
+  };
 
-        {this.state.usuarios.map(usuario => (
-          <Usuario key={usuario.id}
-            usuario={usuario}
-            removerUsuario={this.removerUsuario.bind(this, usuario)}
-          />
-        ))}
-      </>
-    )
-  }
+  const removerUsuario = (usuario) => {
+    if (
+      window.confirm(
+        `Tem certeza que deseja remover "${usuario.nome} ${usuario.sobrenome}"?`
+      )
+    ) {
+      fetch(`https://reqres.in/api/users/${usuario.id}`, {
+        method: "DELETE",
+      }).then((resposta) => {
+        if (resposta.ok) {
+          setUsuarios(usuarios.filter((x) => x.id !== usuario.id));
+        }
+      });
+    }
+  };
+
+  return (
+    <>
+      <AdicionarUsuario adicionarUsuario={adicionarUsuario} />
+
+      {usuarios.map((usuario) => (
+        <Usuario
+          key={usuario.id}
+          usuario={usuario}
+          removerUsuario={() => removerUsuario(usuario)}
+        />
+      ))}
+    </>
+  );
 }
 
-export default Usuarios
+export default Usuarios;
